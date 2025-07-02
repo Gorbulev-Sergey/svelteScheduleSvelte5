c7ec45663f4c87095a2cad2311b02a3990b75bf5
Горбулёв Сергей <gorbulevsv@yandex.ru>
2025-07-02 23:57:18 +0300

    Оформление

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import Title from '$lib/components/Title.svelte';
-	import { Field, type IField } from '$lib/entityes/Field';
+	import { type IField } from '$lib/entityes/Field';
 	import { db } from '$lib/scripts/firebase';
-	import { get, ref, set } from 'firebase/database';
+	import { get, ref } from 'firebase/database';
 
 	// Выбранная дата, год, месяц
 	let date = $state(new Date().toISOString().slice(0, 7));
@@ -50,38 +50,60 @@
 </Title>
 
 <div class="d-flex flex-column mt-3 rounded">
+	<div>
+		<div class="py-2 ps-4 print-title d-none">
+			<b>Расписание на {monthToSring.toUpperCase()} {year} года</b>
+		</div>
+		<div
+			class="no-print"
+			style="display: grid; grid-template-columns: .15fr 1fr; min-height: 2.1em">
+			<div class="text-end px-3" style="min-width: 8.2em;">
+				<span class="badge bg-dark text-light">Дата</span>
+			</div>
+			<div style="display: grid; grid-template-columns: 1fr .1fr .4fr">
+				<div class="text-start ps-2" style="min-width: 13em;">
+					<span class="badge bg-dark text-light">Праздник</span>
+				</div>
+				<div class="text-end" style="min-width: 5em;">
+					<span class="badge bg-dark text-light">Время</span>
+				</div>
+				<div class="text-start ps-4" style="min-width: 8em;">
+					<span class="badge bg-dark text-light">Богослужение</span>
+				</div>
+			</div>
+		</div>
+	</div>
 	{#each Object.entries(dataMonth) as [date, item], i}
 		<div
 			class={`d-flex
 						${new Date(date).getDay() == 0 ? 'bg-primary text-primary bg-opacity-10' : 'bg-light text-dark'}  
 						${i == 0 ? 'rounded-top' : ''} 
 						${i == daysInDataMonth - 1 ? 'rounded-bottom' : ''}`}>
-			<div class={`px-2 py-1 text-nowrap text-end`} style="min-width: 9em;">
-				<div class="d-flex justify-content-center align-items-center flex-column">
-					<b>{Number(date.slice(8, 10))} {monthToStringWithEnd}</b>
-					<div>
-						{new Date(date).toLocaleDateString('Ru-ru', {
-							weekday: 'long'
-						})}
-					</div>
-				</div>
+			<div
+				class="d-flex flex-column justify-content-start align-items-end ps-4 pe-2 py-2"
+				style="min-width: 10em;">
+				<b>{Number(date.slice(8, 10))} {monthToStringWithEnd}</b>
+				<i>
+					{new Date(date).toLocaleDateString('Ru-ru', {
+						weekday: 'long'
+					})}
+				</i>
 			</div>
-			<div class="flex-grow-1 d-flex flex-column">
+			<div class="flex-grow-1 d-flex flex-column text-wrap">
 				{#each item as field, j}
-					<div class="d-flex h-100 align-items-center">
-						<div class="d-flex w-75">
-							<div
-								class={`form-control bg-light bg-transparent border-0 rounded-0 ${getInputColors(date)} my-1`}>
-								{@html dataMonth[date][j].event}
-							</div>
+					<div class="d-flex align-items-start">
+						<div
+							class={`w-100 bg-light bg-transparent border-0 rounded-0 px-3 py-2 ${getInputColors(date)}`}
+							style="width: 50em;">
+							{@html dataMonth[date][j].event}
 						</div>
 						<div
-							class={`form-control bg-transparent border-0 rounded-0 ${getInputColors(date)} ${field.time != '00:00' ? 'fw-bold' : 'text-opacity-25'} my-1`}
+							class={`d-flex justify-content-center align-items-end bg-transparent border-0 rounded-0 py-2 ${getInputColors(date)} fw-bold`}
 							style="width: 4em;">
 							{@html dataMonth[date][j].time}
 						</div>
 						<div
-							class={`form-control bg-transparent border-0 rounded-0 ${getInputColors(date)} my-1`}
+							class={`bg-transparent border-0 rounded-0 px-3 py-2 ${getInputColors(date)}`}
 							style="width: 24em;">
 							{@html dataMonth[date][j].pray}
 						</div>
